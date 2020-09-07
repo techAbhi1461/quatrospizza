91173fa2edc6e9185465123845f38052569f1f9e
techAbhi1461 <tech1461abhi@gmail.com>
2020-09-07 10:14:04 -0400

    Added google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3{
+    font-family: 'Righteous', cursive;
+  }
+
+`
 
 function App() {
   return (
-    <div>Hello Quatros</div>
+    <>
+      <GlobalStyle />
+      <h1>Quatros</h1>
+      <div>Hello Quatros</div>
+    </>
   );
 }
 
