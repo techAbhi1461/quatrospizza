4368725be81619b9069b6fca6ae274cc383bdd2c
techAbhi1461 <tech1461abhi@gmail.com>
2020-09-10 15:23:28 -0400

    Added the hooks files

---
+++ src/Data/FoodData.js
@@ -0,0 +1,52 @@
+export const foodItems = [
+
+    {
+        name: 'Cheese Pizza',
+        img: '/img/pizza.png',
+        section: 'Pizza'
+    },
+    {
+        name: 'Pepperoni Pizza',
+        img: '/img/pizza2.jpeg',
+        section: 'Pizza'
+    },
+    {
+        name: 'Chicken Pizza',
+        img: '/img/chicken-pizza.jpeg',
+        section: 'Pizza'
+    },
+    {
+        name: 'Veggie Pizza',
+        img: '/img/healthy-pizza.jpeg',
+        section: 'Pizza'
+    },
+    {
+        name: 'Burger',
+        img: '/img/burger.jpeg',
+        section: 'Sandwich'
+    },
+    {
+        name: 'Gyro',
+        img: '/img/gyro.jpeg',
+        section: 'Sandwich'
+    },
+    {
+        name: 'Shrimp PoBoy',
+        img: '/img/sandwich.jpeg',
+        section: 'Sandwich'
+    },
+    {
+        name: 'Fries',
+        img: '/img/fries.jpeg',
+        section: 'Sides'
+    }
+
+];
+
+export const foods = foodItems.reduce((res, food) => {
+    if (!res[food.section]) {
+        res[food.section] = [];
+    }
+    res[food.section].push(food);
+    return res;
+}, {})
---
+++ src/Menu/FoodGrid.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Title } from '../Styles/title';
+
+export const FoodGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+`
+
+export const FoodLabel = styled(Title) `
+    position: absolute;
+    background-color: rgba(256, 256, 256, 0.8);
+    padding: 5px;
+`
+
+export const Food = styled.div`
+    height: 100px;
+    padding: 10px;
+    font-size: 20px;
+    background-image: ${({ img }) => `url(${img});`}
+    background-position: center;
+    background-size: cover;
+    filter: contrast(75%);
+    border-radius: 7px;
+    margin-top: 5px;
+    box-shadow: 0px 0px 2px 0px grey;
+    transition-property: box-shadow margin-top filter;
+    transition-duration: .1s;
+
+    &:hover{
+        cursor: pointer;
+        filter: contrast(100%);
+        margin-top: 0px;
+        margin-botom: 5px;
+        box-shadow: 0px 0px 10px 0px grey;
+    }
+`
