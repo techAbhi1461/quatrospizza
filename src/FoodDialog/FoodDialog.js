003a39f6c0aee6916d73e444fb60e6334c07b05f
techAbhi1461 <tech1461abhi@gmail.com>
2020-09-22 12:31:09 -0400

    Added Edit and Delete item feature for the user

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -95,6 +95,8 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
     const quantity = useQuantity(openFood && openFood.quantity);
     const toppings = useToppings(openFood.toppings);
     const choiceRadio = useChoice(openFood.choice);
+    const isEditing = openFood.index > -1;
+
 
     function close() {
         setOpenFood();
@@ -106,6 +108,13 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
         toppings: toppings.toppings,
         choise: choiceRadio.value
     };
+
+    function editOrder() {
+        const newOrders = [...orders];
+        newOrders[openFood.index] = order;
+        setOrders(newOrders);
+        close();
+    }
 
     function addToOrder() {
         setOrders([...orders, order]);
@@ -130,8 +139,9 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
                     {openFood.choices && <Choices openFood={openFood} choiceRadio={choiceRadio} />}
                 </DialogContent>
                 <DialogFooter>
-                    <ConfirmButton onClick={addToOrder} disabled={openFood.choices && !choiceRadio.value}>
-                        Add to order: {formatPrice(getPrice(order))}
+                    <ConfirmButton onClick={isEditing ? editOrder : addToOrder} disabled={openFood.choices && !choiceRadio.value}>
+                        {isEditing ? `Update order: ` : `Add to order: `}
+                        {formatPrice(getPrice(order))}
                     </ConfirmButton>
                 </DialogFooter>
             </Dialog>
