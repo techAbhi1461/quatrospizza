c27f56b73e5ecd0e15b6fba12962a3455d3a14e4
techAbhi1461 <tech1461abhi@gmail.com>
2020-09-27 22:05:17 -0400

    Updates

---
+++ precache-manifest.dbc9ad80b8484fab9409540ee5e45324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3103482c21e59fd9897b7475dc851db",
+    "url": "/quatrospizza/index.html"
+  },
+  {
+    "revision": "35cf45e587ee05ec6cd5",
+    "url": "/quatrospizza/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "367817881e15ec65c485",
+    "url": "/quatrospizza/static/js/2.ddca2095.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quatrospizza/static/js/2.ddca2095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cf45e587ee05ec6cd5",
+    "url": "/quatrospizza/static/js/main.dd74188e.chunk.js"
+  },
+  {
+    "revision": "d5a559f696479db13270",
+    "url": "/quatrospizza/static/js/runtime-main.4fd78ff4.js"
+  }
+]);
