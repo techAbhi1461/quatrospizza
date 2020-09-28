bcf36dedcb4614fd737c312e2b53be0a93278a35
techAbhi1461 <tech1461abhi@gmail.com>
2020-09-28 15:48:59 -0400

    Changed the image extensions to uppercase for deployment

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Banner = styled.div`
     height: 200px;
-    background-image: url(${process.env.PUBLIC_URL + "/img/banner.jpeg"});
+    background-image: url(${process.env.PUBLIC_URL + "/img/banner.JPEG"});
     background-position: center;
     background-size: cover;
     filter: contrast(75%);
